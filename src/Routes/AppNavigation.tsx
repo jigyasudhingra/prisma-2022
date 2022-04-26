6f7f12bb872a8ec8fd3aa16184219d614736f812
Jigyasu Dhingra <68184200+jigyasudhingra@users.noreply.github.com>
2022-04-26 14:12:34 +0530

    Changed to hashrouter

--- src/Routes/AppNavigation.tsx
+++ src/Routes/AppNavigation.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import { LinearProgress } from '@material-ui/core';
 import ContactMe from 'Screens/ContactMe';
 import HomeScreen from 'Screens/HomeScreen';
+import Page404 from 'Screens/Page404';
 
 const Appnavigation: FC = () => {
   return (
@@ -11,7 +12,7 @@ const Appnavigation: FC = () => {
         <Switch>
           <Route path="/" exact component={HomeScreen} />
           <Route path="/register" exact component={ContactMe} />
-          <Route path="/*" exact component={HomeScreen} />
+          <Route path="/*" exact component={Page404} />
         </Switch>
       </Suspense>
     </>
