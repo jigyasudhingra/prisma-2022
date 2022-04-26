07feb6fda8417705551e9bad405a3d2454711b7a
Jigyasu Dhingra <68184200+jigyasudhingra@users.noreply.github.com>
2022-04-26 13:51:22 +0530

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import Theme from './Theme';
 import RootLayout from './Screens/RootLayout';
 import Boot from './Boot';
 
 const App: React.FC = () => {
-  Boot().then(() => { });
+  Boot().then(() => {});
   return (
-    <BrowserRouter>
+    <HashRouter>
       <MuiThemeProvider theme={Theme}>
         <RootLayout />
       </MuiThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
