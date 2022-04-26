e4614b679b3cf76e1a82eeab91fecec9fb5ef9ec
Jigyasu Dhingra <68184200+jigyasudhingra@users.noreply.github.com>
2022-04-26 13:59:12 +0530

    Updates

---
+++ precache-manifest.ebdb2d8f2a8f177ce5bb7a40a328a918.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b83be7df4e365f08e78a21df2f43c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3abad91597f552130f6a",
+    "url": "/static/css/2.6c2fb8fe.chunk.css"
+  },
+  {
+    "revision": "e41c99f72fe4c0c24572",
+    "url": "/static/css/main.38eea998.chunk.css"
+  },
+  {
+    "revision": "3abad91597f552130f6a",
+    "url": "/static/js/2.cd1be9be.chunk.js"
+  },
+  {
+    "revision": "b864f1a35d30a6608bda50ce6703494f",
+    "url": "/static/js/2.cd1be9be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41c99f72fe4c0c24572",
+    "url": "/static/js/main.4f629594.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "45ecd530969074447f77383e22c1f054",
+    "url": "/static/media/AboutPrismaBackground.45ecd530.png"
+  },
+  {
+    "revision": "42669f00674d623a419f399ef1bbdc62",
+    "url": "/static/media/ArtistSectionBackground.42669f00.png"
+  },
+  {
+    "revision": "22c680a15e0a06c52bb0cf19789e3051",
+    "url": "/static/media/ParmishVerma.22c680a1.png"
+  },
+  {
+    "revision": "d664871de4f74eb8dfd1deec33ab75a8",
+    "url": "/static/media/PrismaHome2.d664871d.mp4"
+  },
+  {
+    "revision": "0491d2b319e643f70d5bc72f17b74194",
+    "url": "/static/media/PrismaLogo-AboutSection.0491d2b3.png"
+  },
+  {
+    "revision": "3883da3eaa9ca6056ebfbfc59b29875f",
+    "url": "/static/media/PrismaLogo.3883da3e.png"
+  },
+  {
+    "revision": "d2b90f978e5b404c14b87178858949db",
+    "url": "/static/media/RegistrationEnquiryQR.d2b90f97.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
