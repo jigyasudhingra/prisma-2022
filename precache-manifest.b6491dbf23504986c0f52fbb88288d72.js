c1a77b7d4976c4c7a23d36af6f95f1e4e557d25e
Devansh Verma <vermashubham5000@gmail.com>
2022-04-30 21:41:55 +0530

    Updates

---
+++ precache-manifest.b6491dbf23504986c0f52fbb88288d72.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0985f2e2b43040009028585fbc1f63aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99742f6b4b16db97bb83",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "3e982f92bbfb2b87e729",
+    "url": "/static/css/main.38eea998.chunk.css"
+  },
+  {
+    "revision": "99742f6b4b16db97bb83",
+    "url": "/static/js/2.98e67db3.chunk.js"
+  },
+  {
+    "revision": "210624a0a594ab74a015b9c41c06be53",
+    "url": "/static/js/2.98e67db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e982f92bbfb2b87e729",
+    "url": "/static/js/main.4fa1c2c5.chunk.js"
+  },
+  {
+    "revision": "7736a5055678f1cdb6e6",
+    "url": "/static/js/runtime-main.08188522.js"
+  },
+  {
+    "revision": "45ecd530969074447f77383e22c1f054",
+    "url": "/static/media/AboutPrismaBackground.45ecd530.png"
+  },
+  {
+    "revision": "f0c685ddffdd1939d2a9bc09319b47f3",
+    "url": "/static/media/ArtistSectionBackground.f0c685dd.png"
+  },
+  {
+    "revision": "22c680a15e0a06c52bb0cf19789e3051",
+    "url": "/static/media/ParmishVerma.22c680a1.png"
+  },
+  {
+    "revision": "d664871de4f74eb8dfd1deec33ab75a8",
+    "url": "/static/media/PrismaHome2.d664871d.mp4"
+  },
+  {
+    "revision": "0491d2b319e643f70d5bc72f17b74194",
+    "url": "/static/media/PrismaLogo-AboutSection.0491d2b3.png"
+  },
+  {
+    "revision": "3883da3eaa9ca6056ebfbfc59b29875f",
+    "url": "/static/media/PrismaLogo.3883da3e.png"
+  },
+  {
+    "revision": "d2b90f978e5b404c14b87178858949db",
+    "url": "/static/media/RegistrationEnquiryQR.d2b90f97.png"
+  },
+  {
+    "revision": "f4566a72e86761ff5493f3b4b7f63868",
+    "url": "/static/media/TicketBanner.f4566a72.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c15ca2b891351d0b8a181fd13db4871.js"
+  "/precache-manifest.b6491dbf23504986c0f52fbb88288d72.js"
 );
 
 self.addEventListener('message', (event) => {
