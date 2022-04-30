fd51fe7bf139e286215774d79bba5f8964201f62
Devansh Verma <vermashubham5000@gmail.com>
2022-04-30 14:04:46 +0530

    Updates

---
+++ precache-manifest.e3472ab3a81d4b2146ae81d767fd98ec.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69031c1b3bd693e186ffd80b73e7a8a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39b68f9ae8eec6260cfb",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "cbcf5ad331bec6f7b792",
+    "url": "/static/css/main.38eea998.chunk.css"
+  },
+  {
+    "revision": "39b68f9ae8eec6260cfb",
+    "url": "/static/js/2.1975e0b0.chunk.js"
+  },
+  {
+    "revision": "842e13496774e5ed0d323bc5664828ba",
+    "url": "/static/js/2.1975e0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbcf5ad331bec6f7b792",
+    "url": "/static/js/main.623608ee.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "45ecd530969074447f77383e22c1f054",
+    "url": "/static/media/AboutPrismaBackground.45ecd530.png"
+  },
+  {
+    "revision": "f0c685ddffdd1939d2a9bc09319b47f3",
+    "url": "/static/media/ArtistSectionBackground.f0c685dd.png"
+  },
+  {
+    "revision": "22c680a15e0a06c52bb0cf19789e3051",
+    "url": "/static/media/ParmishVerma.22c680a1.png"
+  },
+  {
+    "revision": "d664871de4f74eb8dfd1deec33ab75a8",
+    "url": "/static/media/PrismaHome2.d664871d.mp4"
+  },
+  {
+    "revision": "0491d2b319e643f70d5bc72f17b74194",
+    "url": "/static/media/PrismaLogo-AboutSection.0491d2b3.png"
+  },
+  {
+    "revision": "3883da3eaa9ca6056ebfbfc59b29875f",
+    "url": "/static/media/PrismaLogo.3883da3e.png"
+  },
+  {
+    "revision": "d2b90f978e5b404c14b87178858949db",
+    "url": "/static/media/RegistrationEnquiryQR.d2b90f97.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/prisma-2022/precache-manifest.9f768ed2e30e721165406818011df412.js"
+  "/precache-manifest.e3472ab3a81d4b2146ae81d767fd98ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prisma-2022/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
