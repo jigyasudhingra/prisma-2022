2a9b68d8096045757e5ca73ef937ac94690584d1
Jigyasu Dhingra <68184200+jigyasudhingra@users.noreply.github.com>
2022-05-03 10:00:34 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc6443dfad7faec1504643eca46d0bc9.js"
+  "/precache-manifest.62bf08a5fa7f3eb659c50aafd8d11aac.js"
 );
 
 self.addEventListener('message', (event) => {
