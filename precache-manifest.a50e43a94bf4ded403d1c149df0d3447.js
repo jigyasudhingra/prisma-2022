da400c448084257b4cb85ddaaba8ef5c2bfb03d5
jigyasudhingra <68184200+jigyasudhingra@users.noreply.github.com>
2023-05-08 23:29:12 +0530

    Updates

---
+++ precache-manifest.a50e43a94bf4ded403d1c149df0d3447.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab04455bd34caa581514e2d19173182",
+    "url": "/prisma-2022/index.html"
+  },
+  {
+    "revision": "9f8da569971e491ba374",
+    "url": "/prisma-2022/static/css/2.753fdb7f.chunk.css"
+  },
+  {
+    "revision": "4c03ac600319c7b4df3c",
+    "url": "/prisma-2022/static/css/main.63fc3bf5.chunk.css"
+  },
+  {
+    "revision": "9f8da569971e491ba374",
+    "url": "/prisma-2022/static/js/2.083836ba.chunk.js"
+  },
+  {
+    "revision": "b864f1a35d30a6608bda50ce6703494f",
+    "url": "/prisma-2022/static/js/2.083836ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c03ac600319c7b4df3c",
+    "url": "/prisma-2022/static/js/main.5936704d.chunk.js"
+  },
+  {
+    "revision": "e48d812fe05072bc3a6d",
+    "url": "/prisma-2022/static/js/runtime-main.645f2cdf.js"
+  },
+  {
+    "revision": "45ecd530969074447f77383e22c1f054",
+    "url": "/prisma-2022/static/media/AboutPrismaBackground.45ecd530.png"
+  },
+  {
+    "revision": "f0c685ddffdd1939d2a9bc09319b47f3",
+    "url": "/prisma-2022/static/media/ArtistSectionBackground.f0c685dd.png"
+  },
+  {
+    "revision": "d07661d25a89efae9ec3ae8af91fedb1",
+    "url": "/prisma-2022/static/media/Aryan.d07661d2.jpg"
+  },
+  {
+    "revision": "7a03deff2ed48557fcc2ba67778d1499",
+    "url": "/prisma-2022/static/media/Baljiwan.7a03deff.jpeg"
+  },
+  {
+    "revision": "1b80e68fb6d9915279b3584ed6fc3c70",
+    "url": "/prisma-2022/static/media/MacV.1b80e68f.jpg"
+  },
+  {
+    "revision": "2e117be878bbf8c4794fc268268d0080",
+    "url": "/prisma-2022/static/media/MojoLand.2e117be8.jpg"
+  },
+  {
+    "revision": "22c680a15e0a06c52bb0cf19789e3051",
+    "url": "/prisma-2022/static/media/ParmishVerma.22c680a1.png"
+  },
+  {
+    "revision": "d664871de4f74eb8dfd1deec33ab75a8",
+    "url": "/prisma-2022/static/media/PrismaHome2.d664871d.mp4"
+  },
+  {
+    "revision": "0491d2b319e643f70d5bc72f17b74194",
+    "url": "/prisma-2022/static/media/PrismaLogo-AboutSection.0491d2b3.png"
+  },
+  {
+    "revision": "3883da3eaa9ca6056ebfbfc59b29875f",
+    "url": "/prisma-2022/static/media/PrismaLogo.3883da3e.png"
+  },
+  {
+    "revision": "b23331a9b1f79e11c5cf130823626770",
+    "url": "/prisma-2022/static/media/RaghavQRCode.b23331a9.jpeg"
+  },
+  {
+    "revision": "0796a886767a08b4bc058c1bc2bd102d",
+    "url": "/prisma-2022/static/media/TIMEX.0796a886.jpg"
+  },
+  {
+    "revision": "6ef5960226e687cd45c1c14924d128a9",
+    "url": "/prisma-2022/static/media/TicketBanner.6ef59602.webp"
+  },
+  {
+    "revision": "aa960503131002b5da6abbf5deec97b0",
+    "url": "/prisma-2022/static/media/bhavjot.aa960503.jpeg"
+  },
+  {
+    "revision": "e02a048fc9da1f7925943e64e26a6b96",
+    "url": "/prisma-2022/static/media/devansh.e02a048f.jpeg"
+  },
+  {
+    "revision": "3052a2cf6825beeb7e01d04a63d3cf5a",
+    "url": "/prisma-2022/static/media/dipanshu.3052a2cf.jpeg"
+  },
+  {
+    "revision": "b98fbfc26df369e4d1365ceed1d8836e",
+    "url": "/prisma-2022/static/media/esha.b98fbfc2.jpeg"
+  },
+  {
+    "revision": "f63b4e7fb98ae45bf73d169774c30642",
+    "url": "/prisma-2022/static/media/jigyasu.f63b4e7f.jpeg"
+  },
+  {
+    "revision": "ae4c17e024bd3bfcf889ffc85bb15d4d",
+    "url": "/prisma-2022/static/media/manya.ae4c17e0.jpeg"
+  },
+  {
+    "revision": "4bad043ef8b5914d5f8785e5ed56225e",
+    "url": "/prisma-2022/static/media/raghav.4bad043e.jpeg"
+  },
+  {
+    "revision": "ef313719505e1dea73b92ff01249fcea",
+    "url": "/prisma-2022/static/media/sakaar.ef313719.jpeg"
+  },
+  {
+    "revision": "ddb2600319754816fc47e35e2c95ea0b",
+    "url": "/prisma-2022/static/media/shagun.ddb26003.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/prisma-2022/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/prisma-2022/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/prisma-2022/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/prisma-2022/static/media/slick.f97e3bbf.svg"
+  }
+]);
